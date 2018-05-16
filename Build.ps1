2a41d45dabd1fb6197965be708bc860e6cb766e9
Artiom <kuchuk.artiom@gmail.com>
2018-05-16 22:33:31 +0200

    Updated build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,19 +1,47 @@
-# Clean
-if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
+# Taken from psake https://github.com/psake/psake
 
-# Initialize
-EnsurePsbuildInstalled
+<#
+.SYNOPSIS
+  This is a helper function that runs a scriptblock and checks the PS variable $lastexitcode
+  to see if an error occcured. If an error is detected then an exception is thrown.
+  This function allows you to run command-line programs without having to
+  explicitly check the $lastexitcode variable.
+.EXAMPLE
+  exec { svn info $repository_trunk } "Error executing SVN. Please verify SVN command-line client is installed"
+#>
+function Exec
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
+        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    )
+    & $cmd
+    if ($lastexitcode -ne 0) {
+        throw ("Exec: " + $errorMessage)
+    }
+}
+
+if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 
 exec { & dotnet restore }
 
-# Build
-Invoke-MSBuild
+$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
+$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
+$commitHash = $(git rev-parse --short HEAD)
+$buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
+$versionSuffix = @{ $true = "--version-suffix=$($suffix)"; $false = ""}[$suffix -ne ""]
 
-# Test
-exec { & dotnet test .\test\MediatR.Tests -c Release }
+echo "build: Package version suffix is $suffix"
+echo "build: Build version suffix is $buildSuffix"
 
-# Package
-$revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
+exec { & dotnet build .\DataPowerTools.sln -c Release --version-suffix=$buildSuffix }
 
-exec { & dotnet pack .\src\MediatR -c Release -o .\artifacts --version-suffix=$revision }
+Push-Location -Path .\test\MediatR.Tests
+
+exec { & dotnet xunit -configuration Release --no-build }
+
+#Pop-Location
+
+exec { & dotnet pack .\DataPowerTools\DataPowerTools.csproj -c Release -o ..\artifacts --include-symbols --no-build $versionSuffix }
