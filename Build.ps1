7b284150b68345c7a964e2148ff06bbea38701be
Artiom <kuchuk.artiom@gmail.com>
2018-06-27 22:38:18 +0200

    Fixed formatting

--- Build.ps1
+++ Build.ps1
@@ -12,27 +12,31 @@
 function Exec
 {
     [CmdletBinding()]
-    param(
-        [Parameter(Position=0,Mandatory=1)][scriptblock]$cmd,
-        [Parameter(Position=1,Mandatory=0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
+    param (
+        [Parameter(Position = 0, Mandatory = 1)][scriptblock]$cmd,
+        [Parameter(Position = 1, Mandatory = 0)][string]$errorMessage = ($msgs.error_bad_command -f $cmd)
     )
     & $cmd
-    if ($lastexitcode -ne 0) {
+    if ($lastexitcode -ne 0)
+    {
         throw ("Exec: " + $errorMessage)
     }
 }
-
-if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
+ 
+if (Test-Path .\artifacts)
+{
+    Remove-Item .\artifacts -Force -Recurse
+}
 
 exec { & dotnet restore }
 
-$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
+$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $( git symbolic-ref --short -q HEAD ) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
 $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
-$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
-$commitHash = $(git rev-parse --short HEAD)
-$buildSuffix = @{ $true = "$($suffix)-$($commitHash)"; $false = "$($branch)-$($commitHash)" }[$suffix -ne ""]
+$suffix = @{ $true = ""; $false = "$($branch.Substring(0,[math]::Min(10, $branch.Length)) )-$revision" }[$branch -eq "master" -and $revision -ne "local"]
+$commitHash = $( git rev-parse --short HEAD )
+$buildSuffix = @{ $true = "$( $suffix )-$( $commitHash )"; $false = "$( $branch )-$( $commitHash )" }[$suffix -ne ""]
 
 
-exec { & dotnet build .\DataPowerTools.sln -c Release --version-suffix=$buildSuffix }
+exec { & dotnet build .\DataPowerTools.sln -c Release --version-suffix =$buildSuffix }
 
-exec { & dotnet pack .\DataPowerTools\DataPowerTools.csproj -c Release -o ..\artifacts --include-symbols --no-build}
+exec { & dotnet pack .\DataPowerTools\DataPowerTools.csproj -c Release -o ..\artifacts --include-symbols --no-build }
