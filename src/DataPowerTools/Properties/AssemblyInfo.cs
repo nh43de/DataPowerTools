9704803e8a0349872d5fd9c7b1416e7c51abf709
Nathan Hollis <natehollis@gmail.com>
2020-09-11 12:43:52 -0500

    Morre build fixes

--- src/DataPowerTools/Properties/AssemblyInfo.cs
+++ src/DataPowerTools/Properties/AssemblyInfo.cs
@@ -6,8 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: InternalsVisibleTo("DataPowerTools.Tests")]
-
 [assembly: AssemblyTitle("DataReaderPowerTools")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
