3191e25aac0e893864463f433b264d3ad8550ff9
Nathan Hollis <natehollis@gmail.com>
2020-09-11 21:01:25 -0500

    CI tweaks

--- Build.ps1
+++ Build.ps1
@@ -47,5 +47,7 @@ foreach ($sample in $samples) {
 }
 #>
 
-exec { & dotnet pack .\src\DataPowerTools\DataPowerTools.csproj -c Release -o $artifacts --include-symbols --no-build }
-exec { & dotnet pack .\src\DataPowerTools.Connectivity\DataPowerTools.Connectivity.csproj -c Release -o $artifacts --include-symbols --no-build }
+
+exec { & dotnet pack .\src\DataPowerTools\DataPowerTools.csproj -c $(Configuration) -o $artifacts --include-symbols --no-build }
+
+exec { & dotnet pack .\src\DataPowerTools.Connectivity\DataPowerTools.Connectivity.csproj -c $(Configuration) -o $artifacts --include-symbols --no-build }
