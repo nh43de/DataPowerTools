f2664c234fb90c9381a608dd0c084756dd123db0
Nathan Hollis <natehollis@gmail.com>
2023-03-31 16:36:52 -0500

    Add ExceptionExtensions back

---
+++ src/DataPowerTools/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataPowerTools.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static string ConcatenateInners(this Exception ex)
+        {
+            var rtn = ex.Message + " " + string.Join(" ",  ex.GetAllInnerExceptions().Select(e => e.Message).ToArray());
+
+            return string.IsNullOrWhiteSpace(rtn) ? "Unknown exception ocurred." : rtn;
+        }
+
+        public static IEnumerable<Exception> GetAllInnerExceptions(this Exception ex)
+        {
+            var iex = ex.InnerException;
+            while (iex != null)
+            {
+                yield return iex;
+                iex = iex.InnerException;
+            }
+        }
+    }
+}
