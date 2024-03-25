124a3384a3eab066baa3c8b22e3d7aec7e39867a
Nathan Hollis <natehollis@gmail.com>
2024-03-25 11:08:35 -0500

    Add enum extensions and enum flag extensions

---
+++ src/DataPowerTools/Extensions/EnumExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataPowerTools.Extensions
+{
+    /// <summary>
+    /// Provides useful extensions for enumerations.
+    /// </summary>
+    public static class EnumExtensions
+    {
+        public static T ParseEnumType<T>(this string enumString)
+        {
+            return (T) Enum.Parse(typeof(T), enumString);
+        }
+        
+        //public static string WriteEnumAsString<TEnum>(TEnum tEnum) where TEnum : Enum
+        //{
+        //    return tEnum.ToString();
+        //}
+
+        //public static string[] GetEnumOptionsAsStringArray<TEnum>(this TEnum t) where TEnum : Enum
+        //{
+        //    return Enum.GetNames(typeof(TEnum));
+        //}
+    }
+}
