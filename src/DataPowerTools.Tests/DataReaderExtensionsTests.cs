002a438989431ac9bce202bcf0ac4c71a424777b
Nathan Hollis <natehollis@gmail.com>
2024-12-11 21:00:43 -0600

    ws

--- src/DataPowerTools.Tests/DataReaderExtensionsTests.cs
+++ src/DataPowerTools.Tests/DataReaderExtensionsTests.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.PortableExecutable;
 using DataPowerTools.DataReaderExtensibility.TransformingReaders;
 using DataPowerTools.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
